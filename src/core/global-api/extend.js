8183616eaf16f68ec55bbe06e4005767102b10ad
XFcall <997172716@qq.com>
2021-11-23 18:18:39 +0800

    glaobal-api解析

--- src/core/global-api/extend.js
+++ src/core/global-api/extend.js
@@ -33,6 +33,7 @@ export function initExtend (Vue: GlobalAPI) {
     const Sub = function VueComponent (options) {
       this._init(options)
     }
+    // 指向Vue原型
     Sub.prototype = Object.create(Super.prototype)
     Sub.prototype.constructor = Sub
     Sub.cid = cid++
